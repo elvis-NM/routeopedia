4d4b0c3526c74f0e95cb489abd47fb2bb830f3b3
Elvis Makia <elvis_makia@yahoo.com>
2024-07-10 19:50:02 -0400

    3 nav options implemented   commit

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -26,19 +26,30 @@ function Header() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active text-danger" : "nav-link"
+                  }
+                  aria-current="page"
+                  to="/"
+                >
                   Home
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active text-danger" : "nav-link"
+                  }
+                  to="/about"
+                >
                   About
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/cryptodetail/BTC/10">
+                <NavLink className="nav-link" to="/cryptodetail/BTC/10">
                   Crypto Detail
-                </Link>
+                </NavLink>
               </li>
               <li className="nav-item dropdown">
                 <a
