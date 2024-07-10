760cb1324c272d616093025fffadf78bae0f081a
Elvis Makia <elvis_makia@yahoo.com>
2024-07-10 16:42:17 -0400

    first commit

---
+++ src/Pages/ProductDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProductDetails() {
+  return <div>Details</div>;
+}
+
+export default ProductDetails;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import Header from "./Layout/Header";
+import Footer from "./Layout/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import CryptoDetails from "./CryptoDetails";
+import Product from "./Pages/Product";
+import ProductList from "./Pages/ProductList";
+import ProductDetails from "./Pages/ProductDetails";
+import CreateProduct from "./Pages/CreateProduct";
+import NotFound from "./NotFound";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div style={{ backgroundColor: "black", color: "grey" }}>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="product">
+          <Route path="" element={<Product />}></Route>
+          <Route path="list" element={<ProductList />}></Route>
+          <Route path="create" element={<CreateProduct />}></Route>
+          <Route path="details" element={<ProductDetails />}></Route>
+        </Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+    <div className="px-4"></div>
+    <Footer />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
